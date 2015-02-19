2a1652d06ac321b72b15ce7536f57e506966ce9c
ghoshbishakh <ghoshbishakh@gmail.com>
2015-02-19 12:14:23 +0530

    templating

--- profiles/forms.py
+++ profiles/forms.py
@@ -7,7 +7,7 @@ class UserForm(ModelForm):
 
     class Meta:
         model = User
-        fields = ['first_name', 'last_name']
+        fields = ['first_name', 'last_name','email']
 
 
 class CreateUserForm(UserCreationForm):
